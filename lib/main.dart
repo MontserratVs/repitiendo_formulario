ea0e1a039ca8a3af87e83d680ef9ee665a60bddf
Montserrat vs. <mvelarde2023@incos-santacruz.edu.bo>
2025-06-03 19:43:25 -0400

    Implementacion del Titulo de mi formulario de registro

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,9 @@ class MyApp extends StatelessWidget{
 }
 @override
 Widget build(BuildContext context) {
-
+ //TODO:implement build
+  // throw UnimplementedError();
+  return MaterialApp(
+    title:"Formulario de Registro",
+  )
 }
